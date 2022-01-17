0ee44dbabcea09b659d73bcc056fe6c931185f0a
Milica Milakovic <milakovicmilica@yahoo.com>
2022-01-17 19:54:59 +0100

    Create a copy of the certificate on Desktop

--- src/main/java/net/etfbl/krz/cryptoquiz/RegistrationController.java
+++ src/main/java/net/etfbl/krz/cryptoquiz/RegistrationController.java
@@ -3,7 +3,6 @@ package net.etfbl.krz.cryptoquiz;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import net.etfbl.krz.model.Certificate;
 import net.etfbl.krz.model.Player;
 import net.etfbl.krz.model.SecurityUtil;
@@ -12,7 +11,7 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 public class RegistrationController {
@@ -35,11 +34,26 @@ public class RegistrationController {
         try {
             if(usernameAvailable(player.getUsername())){
                 String hash = SecurityUtil.hashFunction(player.getUsername());
+                hash = hash.replace("/","%2F");
+
                 new File(Main.playersDir+File.separator+hash).mkdir();
                 File outputDir= new File(Main.playersDir+File.separator+ hash+File.separator);
                 Certificate.issueUserCertificate(player, outputDir);
+
+                // Kreira se kopija sertifikata koja ce biti dostupna korisniku na desktopu,
+                // kako korisnik ne bi pristupao direktorijumu sa sertifikatima drugih korisnika
+                String desktop = System.getProperty("user.home") + "/Desktop";
+                Files.copy(Paths.get(outputDir+File.separator+player.getUsername()+".cer"),Paths.get(desktop+File.separator+player.getUsername()+".cer"), StandardCopyOption.REPLACE_EXISTING);
+                certificatePath.setText(desktop+File.separator+player.getUsername()+".cer");
+                username.setText("");
+                password.setText("");
+                email.setText("");
+
             } else
+            {
                 System.out.println("Korisnicko ime je zauzeto");
+                certificatePath.setText("Korisničko ime je zauzeto!");
+            }
 
         } catch (Exception e){
             e.printStackTrace();
@@ -48,6 +62,10 @@ public class RegistrationController {
 
     private boolean usernameAvailable(String username){
         String hash = SecurityUtil.hashFunction(username);
+
+        hash = hash.replace("/","%2F");
+        System.out.println(username+"=" + hash);
+
         List<String> players;
         try {
             players = Files.readAllLines(Paths.get(Main.playersList));
